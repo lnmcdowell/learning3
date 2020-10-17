be6fad778fe925a91b71432fe1cd688b8ba6aed7
Larry Nathaniel Mcdowell <nathaniel29651@gmail.com>
2020-10-17 05:48:14 -0400

    Initial Commit

---
+++ learning3/learning3App.swift
@@ -0,0 +1,17 @@
+//
+//  learning3App.swift
+//  learning3
+//
+//  Created by Larry Mcdowell on 10/17/20.
+//
+
+import SwiftUI
+
+@main
+struct learning3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
